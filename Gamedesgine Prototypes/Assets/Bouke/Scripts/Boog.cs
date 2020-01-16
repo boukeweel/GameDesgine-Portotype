0d027a3be0f0ad64078e71b12aa80afb9c3e8ba1
Bouke <BoukeWeel@outlook.com>
2020-01-16 12:04:25 +0100

    veder met boog en begonen met speer

---
+++ Gamedesgine Prototypes/Assets/Bouke/Scripts/Boog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boog : MonoBehaviour
+{
+    public float max_loading_time;
+    public GameObject Arrow;
+
+    public float xmouse;
+
+    public Player speler;
+
+    public Animator animator;
+
+    private void Update()
+    {
+
+        xmouse = Input.GetAxis("Mouse X") * speler.mousesensetifity;
+        if (Input.GetMouseButtonDown(0))
+        {
+            //animator.SetBool("MouseDown", true);
+            Instantiate(Arrow, transform.position, transform.rotation * Quaternion.Euler(0, xmouse, 0));
+
+
+        }
+
+        //if(animator.GetBool("MouseDown") == true)
+        //{
+            
+            
+        //}
+    }
+
+}
